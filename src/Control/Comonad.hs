60b1c9f8456462fd6178beb7a9e5306c26e1c848
Edward Kmett <ekmett@gmail.com>
2015-03-17 04:54:57 -0400

    minor CPP cleanup

--- src/Control/Comonad.hs
+++ src/Control/Comonad.hs
@@ -1,10 +1,8 @@
 {-# LANGUAGE CPP #-}
-#ifdef __GLASGOW_HASKELL__
 #if __GLASGOW_HASKELL__ >= 707
 {-# LANGUAGE DeriveDataTypeable, StandaloneDeriving, Safe #-}
 #elif __GLASGOW_HASKELL__ >= 702
 {-# LANGUAGE Trustworthy #-}
-#endif
 #endif
  -----------------------------------------------------------------------------
 -- |
